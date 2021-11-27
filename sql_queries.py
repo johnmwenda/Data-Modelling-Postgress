27fd2a6fcf9f381121192d804f37b7950455bfaa
john <jmwenda56@gmail.com>
2021-11-27 14:03:58 +0300

    Fix

--- sql_queries.py
+++ sql_queries.py
@@ -1,42 +1,12 @@
 # DROP TABLES
 
-songplay_table_drop = "DROP TABLE IF EXISTS songplays"
+songplay_table_drop = "DROP TABLE IF EXISTS songplays CASCADE"
 user_table_drop = "DROP TABLE IF EXISTS users"
 song_table_drop = "DROP TABLE IF EXISTS songs"
 artist_table_drop = "DROP TABLE IF EXISTS artists"
 time_table_drop = "DROP TABLE IF EXISTS time"
 
 # CREATE TABLES
-
-songplay_table_create = ("""
-CREATE TABLE IF NOT EXISTS
-    songplays(
-        songplay_id SERIAL PRIMARY KEY,
-        start_time text null,
-        user_id integer not null,
-        level text not null,
-        song_id text null,
-        artist_id text null,
-        session_id integer not null,
-        location text null,
-        user_agent text not null,
-        CONSTRAINT fk_time
-            FOREIGN KEY(start_time)
-                REFRENCES start(start_time),
-        CONSTRAINT fk_users
-            FOREIGN KEY(user_id)
-                REFRENCES users(user_id),
-        CONSTRAINT fk_songs
-            FOREIGN KEY(song_id)
-                REFRENCES songs(song_id),
-        CONSTRAINT fk_songs
-            FOREIGN KEY(song_id)
-                REFRENCES songs(song_id),
-        CONSTRAINT fk_artist
-            FOREIGN KEY(artist_id)
-                REFRENCES artists(artist_id),
-    )
-""")
 
 user_table_create = ("""
     CREATE TABLE IF NOT EXISTS
@@ -84,6 +54,33 @@ time_table_create = ("""
     )
 """)
 
+songplay_table_create = ("""
+CREATE TABLE IF NOT EXISTS
+    songplays(
+        songplay_id SERIAL PRIMARY KEY,
+        start_time text null,
+        user_id integer not null,
+        level text not null,
+        song_id text null,
+        artist_id text null,
+        session_id integer not null,
+        location text null,
+        user_agent text not null,
+        CONSTRAINT fk_time
+            FOREIGN KEY(start_time)
+                REFERENCES time(start_time),
+        CONSTRAINT fk_users
+            FOREIGN KEY(user_id)
+                REFERENCES users(user_id),
+        CONSTRAINT fk_songs
+            FOREIGN KEY(song_id)
+                REFERENCES songs(song_id),
+        CONSTRAINT fk_artist
+            FOREIGN KEY(artist_id)
+                REFERENCES artists(artist_id)
+    )
+""")
+
 # INSERT RECORDS
 
 songplay_table_insert = ("""
@@ -117,7 +114,7 @@ where s.title=%s and a.name=%s and s.duration=%s
 
 # QUERY LISTS
 
-create_table_queries = [songplay_table_create, user_table_create, song_table_create, artist_table_create, time_table_create]
+create_table_queries = [user_table_create, song_table_create, artist_table_create, time_table_create, songplay_table_create]
 drop_table_queries = [songplay_table_drop, user_table_drop, song_table_drop, artist_table_drop, time_table_drop]
 
 
